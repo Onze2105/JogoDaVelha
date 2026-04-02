2a112f4ebf9136571ad219055ecd6e6cb12979d3
Filipe Dias <filipedias234@gmail.com>
2026-04-01 22:50:40 -0300

    icones adicionados, função de empate adicionada e refinamento geral

--- javaFXteste/src/application/JogoDaVelha.java
+++ javaFXteste/src/application/JogoDaVelha.java
@@ -13,6 +13,8 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class JogoDaVelha extends Application {
 
@@ -20,14 +22,17 @@ public class JogoDaVelha extends Application {
     private boolean turnoX = true;
     private Button[][] botoes;
 
+
     @Override
     public void start(Stage stage) {
+    	stage.getIcons().add(new Image("/tictactoe.png"));
+    	stage.setResizable(false);
+    	stage.setTitle("Jogo da Velha");
     	Label texto = new Label("Bem-vindo ao jogo da velha!");
     	Button iniciar = new Button("Iniciar Jogo");
     	VBox root = new VBox(10);
-    	botoes = new Button[3][3];
-    	
     	root.setAlignment(Pos.CENTER);
+
     	
     	root.getChildren().addAll(texto, iniciar);
     	
@@ -36,6 +41,7 @@ public class JogoDaVelha extends Application {
     	
         GridPane grid = new GridPane();
         Scene jogoScene = new Scene(grid, 300, 300);
+        botoes = new Button[3][3];
         
         iniciar.setOnAction(e -> stage.setScene(jogoScene));
         stage.setScene(menuScene);
@@ -59,7 +65,15 @@ public class JogoDaVelha extends Application {
     
     private void PopupVitoria(String j) {
     	Alert vitoria = new Alert(AlertType.CONFIRMATION, "Deseja jogar de novo?", ButtonType.YES, ButtonType.NO);
-        vitoria.setTitle("Vitória!");
+    	ImageView img = new ImageView(
+    		    new Image(getClass().getResource("/velha.png").toExternalForm())
+    		);
+
+    		img.setFitWidth(50);
+    		img.setFitHeight(50);
+
+    		vitoria.setGraphic(img);
+    	vitoria.setTitle("Vitória!");
         vitoria.setHeaderText("Vitória do " + j + "!");
         vitoria.showAndWait().ifPresent(response ->{
         	if(response == ButtonType.YES) {
@@ -71,6 +85,35 @@ public class JogoDaVelha extends Application {
     	
     }
     
+    private void PopupEmpate() {
+    	Alert empate = new Alert(AlertType.CONFIRMATION, "Deseja jogar de novo?", ButtonType.YES, ButtonType.NO);
+    	ImageView img = new ImageView(
+    		    new Image(getClass().getResource("/velha.png").toExternalForm())
+    		);
+
+    		img.setFitWidth(50);
+    		img.setFitHeight(50);
+
+    		empate.setGraphic(img);
+    	empate.setTitle("Deu velha!");
+    	empate.setHeaderText(null);
+    	empate.showAndWait().ifPresent(response ->{
+    		if(response == ButtonType.YES) {
+    			reiniciarJogo();
+    		} else {
+    			System.exit(0);
+    		}
+    	});
+    }
+    
+    private boolean tabuleiroCheio() {
+    	for (int i = 0; i < tabuleiro.length; i++) {
+    		for(int j = 0; j < tabuleiro.length; j++) {
+    			if(tabuleiro[i][j] == null) return false;
+    		}
+    	}
+    	return true;
+    }
     
 
     private void jogar(Button btn, int linha, int coluna) {
@@ -83,6 +126,9 @@ public class JogoDaVelha extends Application {
         if (verificarVitoria(jogador)) {
             PopupVitoria(jogador);
             return;
+        } else if(tabuleiroCheio()) {
+        	PopupEmpate();
+        	return;
         }
 
         turnoX = !turnoX;
@@ -105,7 +151,7 @@ public class JogoDaVelha extends Application {
         for (int i = 0; i < 3; i++) {
             if (j.equals(tabuleiro[i][0]) &&
                 j.equals(tabuleiro[i][1]) &&
-                j.equals(tabuleiro[i][2])) return true;
+                j.equals(tabuleiro[i][2]))return true;
 
             if (j.equals(tabuleiro[0][i]) &&
                 j.equals(tabuleiro[1][i]) &&
@@ -119,9 +165,12 @@ public class JogoDaVelha extends Application {
         if (j.equals(tabuleiro[0][2]) &&
             j.equals(tabuleiro[1][1]) &&
             j.equals(tabuleiro[2][0])) return true;
-
+        
+       
         return false;
+        
     }
+    
 
     public static void main(String[] args) {
         launch();
